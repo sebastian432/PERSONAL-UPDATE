b64067dd784cc774f569b29d8b262d3113d28c8b
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-11-01 16:20:04 -0500

    Create int main.cpp

---
+++ int main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include "Mejora.h"
+using namespace std;
+
+
+/*
+Mejora::Mejora(string _nombre)
+{
+    nombre=_nombre;
+}
+*/
+
+
+int main()
+{
+    Mejora m1;
+
+    m1.Mostrar_mensaje();
+    m1.Fisica();
+
+return 0;
+}
