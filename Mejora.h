62a1169b28777cb00d26e678b05f214aa183cdb5
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-11-01 16:20:34 -0500

    Create Mejora.h

---
+++ Mejora.h
@@ -0,0 +1,17 @@
+#ifndef MEJORA_H
+#define MEJORA_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+class Mejora
+{
+private:
+        int aspecto_f;
+        string nombre;
+public:
+        void Mostrar_mensaje();
+        void Fisica();
+};
+#endif // MEJORA_H
