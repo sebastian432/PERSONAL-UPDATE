5a34e135bb9d1de4fa2aa788f6e279124fe7dc78
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-11-02 23:43:17 -0500

    Create estadistica.cpp

---
+++ estadistica.cpp
@@ -0,0 +1,12 @@
+#include "Estadistica.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+Estadistica::Estadistica(string _ejercicio,int _repeticiones)
+{
+        ejercicio=_ejercicio;
+        repeticiones=_repeticiones;
+}
+
