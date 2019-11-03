7e2d0775b17d5f12c898a7bcf4ad5b8926557846
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-11-02 23:42:47 -0500

    Create Estadistica.h

---
+++ Estadistica.h
@@ -0,0 +1,19 @@
+#ifndef ESTADISTICA_H
+#define ESTADISTICA_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+class Estadistica
+{
+    private:
+            string ejercicio;
+            int repeticiones;
+    public:
+            Estadistica(string ,int);
+            void Pedirdatos();
+
+};
+
+#endif // ESTADISTICA_H
